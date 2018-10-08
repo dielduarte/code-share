6290802a8e6577ea61845eedb442f112c7907554
Charles Assuncao <junioassuncaocharles@gmail.com>
2018-10-08 21:06:44 +0200

    Create config file

---
+++ src/resources/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  packageJson: '/package.json',
+  defaultEncondig: 'utf8',
+  codeSandBox: {
+    apiUrl: 'https://codesandbox.io/api/v1/sandboxes/define?json=1',
+    shareUrl: 'https://codesandbox.io/s/',
+  },
+};
