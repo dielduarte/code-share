e367fa07621975cfd95cdedfa2a794f1b69517fe
dielduarte <magdiel.kesller@gmail.com>
2018-10-09 23:23:07 +0200

    fix: codesanbox file

--- src/services/codeSandBox/codeSandBox.service.ts
+++ src/services/codeSandBox/codeSandBox.service.ts
@@ -12,16 +12,16 @@ export class CodeSandBox implements ShareService {
   private apiUrl: string = config.codeSandBox.apiUrl;
 
   public shareProject(): void | boolean {
-    const files = FilesManager.getFiles();
-    if (!files) {
+    const projectFiles = FilesManager.getFiles();
+    if (!projectFiles) {
       UtilsService.showMessage('No files found. Please, try again.');
 
       return;
     }
 
-    const parameterApi: any = getParameters({
-      files,
-    });
+    const parameterApi: any = { files: { ...projectFiles } };
+
+    // console.log(parameterApi);
 
     Http.post<CodeSandBoxApi>(this.apiUrl, parameterApi)
       .then(response => {
@@ -30,6 +30,7 @@ export class CodeSandBox implements ShareService {
         );
       })
       .catch(err => {
+        console.log(err);
         UtilsService.showMessage(
           'Something bad happened trying to create the project. Please, try again.'
         );
