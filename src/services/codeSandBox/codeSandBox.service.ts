adf1ecfc346c1d42cfe0edb1caa8bcf00299a546
dielduarte <magdiel.kesller@gmail.com>
2018-10-09 13:45:52 +0200

    feat(share-project): add and use FilesManager.getFiles method

--- src/services/codeSandBox/codeSandBox.service.ts
+++ src/services/codeSandBox/codeSandBox.service.ts
@@ -5,19 +5,15 @@ import { Http } from './../http.service';
 import { UtilsService } from './../utils.service';
 import { CodeSandBoxApi } from './definitions/codeSandBox.api.interface';
 
-const PACKAGEJSON = 'package.json';
 const SANDBOX_ID = 'sandbox_id';
 
 export class CodeSandBox implements ShareService {
   private apiUrl: string = config.codeSandBox.apiUrl;
 
   public shareProject(): void {
+    const files = FilesManager.getFiles();
     const parameterApi: CodeSandBoxApi = {
-      files: {
-        [PACKAGEJSON]: {
-          content: FilesManager.PackageJson,
-        },
-      },
+      files,
     };
 
     Http.post<CodeSandBoxApi>(this.apiUrl, parameterApi)
--- src/services/filesManager.service.ts
+++ src/services/filesManager.service.ts
@@ -1,18 +1,42 @@
 import * as fs from 'fs';
-import * as path from 'path';
 import * as vscode from 'vscode';
 
 import { config } from './../resources/config';
-import { PackageJson } from './definitions/packageJson.interface';
+
+const pathsToIgnore: Array<String> = ['node_modules'];
 
 export class FilesManager {
-  static get PackageJson(): PackageJson {
-    const packageJsonPath = path.join(
-      vscode.workspace.rootPath || '',
-      config.packageJson
-    );
-    return JSON.parse(
-      fs.readFileSync(packageJsonPath, { encoding: config.defaultEncondig })
-    );
+  static getFiles(
+    dirname?: string,
+    allFiles?: { [file: string]: { content: string } }
+  ) {
+    try {
+      const dir = dirname ? dirname : vscode.workspace.rootPath || '';
+      const files = fs.readdirSync(dir);
+      const _allFiles = allFiles || {};
+
+      for (const file of files) {
+        const path = dir + '/' + file;
+
+        if (pathsToIgnore.includes(file)) continue;
+
+        if (fs.statSync(path).isDirectory()) {
+          this.getFiles(path, _allFiles);
+          continue;
+        }
+
+        const content = JSON.stringify(
+          fs.readFileSync(path, {
+            encoding: config.defaultEncondig,
+          })
+        );
+
+        _allFiles[path] = { content };
+      }
+
+      return _allFiles;
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
